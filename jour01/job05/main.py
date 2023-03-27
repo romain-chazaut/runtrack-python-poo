cec5e2be73bed57a57dfe776716f0533f0e32ac5
romain-chazaut <romain.chazaut@laplateforme.io>
2023-03-27 15:45:21 +0200

    first commit

--- jour01/job05/main.py
+++ jour01/job05/main.py
@@ -1,5 +1,21 @@
 class Animal:
-    def __init__(self):
-        self.age = 0
-        self.prenom = ""
+    def __init__(self, age, prenom):
+        self.age = age
+        self.prenom = prenom
+
+    def setvieillir(self, age_to_add):
+        self.age += age_to_add
+        print("L'age de l'animal", self.age)
+
+    def setNom(self, nom_Animal):
+        self.prenom = nom_Animal
+        print("L'animal se nomme", self.prenom)
+
+animal = Animal(0, "")
+
+animal.setvieillir(6)
+animal.setvieillir(8)
+animal.setNom("michel")
+
+
 
