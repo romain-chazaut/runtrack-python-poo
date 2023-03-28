6d397e5796c1ed37a94493173b5cdafec0c5863c
romain-chazaut <romain.chazaut@laplateforme.io>
2023-03-28 16:06:59 +0200

    5th commit

---
+++ jour02/job04/main.py
@@ -0,0 +1,22 @@
+class Forme:
+    def __init__(self):
+        pass
+    
+    def aire(self, aire=0):
+        self.aire = aire
+        return aire
+
+class Rectangle(Forme):
+    def __init__(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur        
+        
+    def aire(self):
+        return self.largeur * self.hauteur        
+
+aire_rectangle = Rectangle(6, 7)
+aire = aire_rectangle.aire()
+print("Aire du rectangle:", aire)
+
+
+
